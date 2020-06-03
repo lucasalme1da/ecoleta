587d5615750e370f3f68610567c3a86baa1bbd09
Lucas Almeida <lucasdealmeida.ss@gmail.com>
2020-06-03 08:13:48 -0300

    Application back-end

---
+++ server/src/database/migrations/01_create_items.ts
@@ -0,0 +1,14 @@
+import Knex from "knex";
+
+export async function up(knex: Knex) {
+  return knex.schema.createTable('items', table => {
+    table.increments('id').primary()
+    table.string('title').notNullable()
+    table.string('image').notNullable()
+  })
+}
+
+export async function down(knex: Knex) {
+  return knex.schema.dropTable('items')
+}
+
